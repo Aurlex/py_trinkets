9341e7e4ff5802fd4d4f800128105ca528b17ce9
aurlex1@gmail.com <aurlex1@gmail.com>
2022-05-11 18:31:45 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="py_trinkets",
-    version="0.1"
+    version="0.1",
     url="https://github.com/Aurlex/py_trinkets",
     author="Aurlex",
     author_email="aurlex1@gmail.com",
