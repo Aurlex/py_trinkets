1a881b1667a82c16da90e99348b84cd429a4a782
aurlex1@gmail.com <aurlex1@gmail.com>
2022-05-11 18:30:05 +0100

    update __init__.py, setup.py

--- py_trinkets/__init__.py
+++ py_trinkets/__init__.py
@@ -1,2 +1,2 @@
-__version__ = "dev"
+__version__ = "0.1"
 
--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
+from py_trinkets import __version__
+
 
 setup(
     name="py_trinkets",
-    version="0.1",
+    version=__version__
     url="https://github.com/Aurlex/py_trinkets",
     author="Aurlex",
     author_email="aurlex1@gmail.com",
