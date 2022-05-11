1d2385808f4f90239c2c7c3101576004f659f1b7
aurlex1@gmail.com <aurlex1@gmail.com>
2022-05-11 18:46:03 +0100

    update __init__.py

--- py_trinkets/__init__.py
+++ py_trinkets/__init__.py
@@ -3,3 +3,7 @@ import py_trinkets.mathematics
 import py_trinkets.tools
 
 __all__ = ["game_dev", "mathematics", "tools", "py_trinkets"]
+
+from py_trinkets.game_dev import *
+from py_trinkets.mathematics import *
+from py_trinkets.tools import *
