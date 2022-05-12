cba51e984c58ca72ff6739b5412dd14b1cf6c96a
aurlex1@gmail.com <aurlex1@gmail.com>
2022-05-12 17:18:17 +0100

    update direction_check.py

--- Examples/Vector2D/direction_check.annotated.py
+++ Examples/Vector2D/direction_check.annotated.py
@@ -1,15 +1,20 @@
+# Import mathematics from the py_trinkets library
 from py_trinkets.m import *
+
+# Function to print the direction components of a vector
 
 
 def direction(vector):
-    if vector.x != 0:
+    if vector.x != 0:  # Direction cannot be 0
+        # Vector2D_RIGHT() is a vector with the value of (1, 0)
         if Vector2D_RIGHT().x <= vector.x:
             print("X: right")
         else:
             print("X: left")
     else:
         print("X: centre")
-    if vector.y != 0:
+    if vector.y != 0:  # Direction cannot be 0
+        # Vector2D_DOWN() is a vector with the value of (0, 1)
         if Vector2D_DOWN().x <= vector.y:
             print("Y: down")
         else:
